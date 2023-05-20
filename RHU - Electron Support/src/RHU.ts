d9955b8119268f19ad250be44fff055a6fed42d4
randomuserhi <magicalworld.nano@gmail.com>
2023-05-20 21:52:10 +0100

    Started implementing RHU

--- RHU - Electron Support/src/RHU.ts
+++ RHU - Electron Support/src/RHU.ts
@@ -1,3 +1,21 @@
-window.RHU = {
-    test: ""
-};
+(function() {
+
+    // Core Implementation for initial import
+    interface Core
+    {
+        
+    }
+    let core : Core;
+    (function(core : Core) {
+
+
+
+    })(core);
+
+    // RHU implementation
+    
+    window.RHU = {
+        test: ""
+    };
+
+})();
