178fdd0191a46489de81fa58334d078a85eebf6c
randomuserhi <magicalworld.nano@gmail.com>
2023-05-23 06:38:45 +0100

    Created blankImg Extension

---
+++ RHU - Typescript/types/extensions/blankImg.d.ts
@@ -0,0 +1,10 @@
+declare global
+{
+    interface RHU
+    {
+
+        blankImg?: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNgYAAAAAMAASsJTYQAAAAASUVORK5CYII=";
+    }
+}
+
+export {}
