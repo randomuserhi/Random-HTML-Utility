b423e76cb0345e6e881e2569d909d508635645a4
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-07-11 17:00:28 +0100

    Deprecated old style system and started implementing new

--- RHU/src/modules/style.ts
+++ RHU/src/modules/style.ts
@@ -9,156 +9,41 @@
             if (RHU.exists(RHU.Style))
                 console.warn("Overwriting RHU.Style...");
 
-            // TODO(randomuserhi): Implement the actual implementation => types seem to work
-            // TODO(randomuserhi): Refactor types to reorganise and rename them => currently a lot of RHU.Style.StyledType<...> & {...}
-            //                     which gets increasingly messy
-
-            // Type aliases to allow for privated symbols
+            // Types for symbols
             interface SymbolCollection
             { 
                 readonly name: unique symbol;
             }
-            type Body = RHU.Style.Body &
-            {
-                [symbols.name]?: string | null;
-            }
-            type Block = RHU.Style.Block;
-            type MediaQuery = RHU.Style.MediaQuery;
             const symbols: SymbolCollection = {
                 name: Symbol("style name"),
             } as SymbolCollection;
 
-            // Utility functions
-            let isPlainObject = function(obj: any): boolean
-            {
-                return Object.getPrototypeOf(obj) === Object.prototype;
-            }
+            // Type aliases for private symbol properties
 
-            RHU.Style = function(arg: any)
-            {
-                // Handle function overloads in this way...
-                if (RHU.exists(new.target)) return new styleBlock(arg);
-                else return new styleBody(arg);
-            } as Function as RHU.Style;
-
-            let styleBlock = function<T extends {}>(this: Block, generator: (style: RHU.Style.StyledType<T, Block> & Block) => void): RHU.Style.StyledType<T, Block> & Block
-            {
-                throw new Error("Not implemented yet.");
-            } as Function as { 
-                new<T extends {}>(generator: (style: RHU.Style.StyledType<T, Block> & Block) => void): RHU.Style.StyledType<T, Block> & Block;
-                prototype: Block;
-            };
-
-            let styleBody = function(this: Body, declaration: RHU.Style.BodyDeclaration)
-            {
-                // Deep clone input into style body
-                let clone: (target: Body, declaration: RHU.Style.BodyDeclaration) => void = function(target, declaration)
-                {
-                    for (let key of Object.keys(declaration))
-                    {
-                        let value = declaration[key];
-                        if (typeof value === "string" || typeof value === "number") target[key] = value;
-                        else if (isMediaQuery(value)) target[key] = new mediaQuery(value);
-                        else if (isStyleBody(value)) target[key] = new styleBody(value);
-                        else if (isPlainObject(value)) target[key] = new styleBody(value as RHU.Style.BodyDeclaration);
-                        // TODO(randomuserhi): Better error message => include full property path?
-                        else throw new Error(`Object assigned to ${key} is not a valid style object.`);
-                    }     
-                };
-                clone(this, declaration);
-            } as Function as { 
-                new(declaration: RHU.Style.BodyDeclaration): Body;
-                prototype: Body;
-            };
-            styleBody.prototype[Symbol.toPrimitive] = function(hint)
-            {
-                if (hint !== "number") return this.toString();
-                return null;
-            };
-            styleBody.prototype.toString = function()
-            {
-                throw new Error("Not implemented yet.");
-            }
-            let isStyleBody: (obj: any) => obj is Body 
-                = Object.isPrototypeOf.bind(styleBody.prototype);
-
-            RHU.Style.MediaQuery = function(arg: any)
-            {
-                if (RHU.exists(new.target)) throw new Error("This function cannot be called with the 'new' keyword.");
-                else return new mediaQuery(arg);
-            } as any;
-
-            let mediaQuery = function(this: MediaQuery, declaration: RHU.Style.BlockDeclaration)
-            {
-                // Deep clone input into media query body
-                let clone: (target: MediaQuery, declaration: RHU.Style.BlockDeclaration) => void = function(target, declaration)
-                {
-                    for (let key of Object.keys(declaration))
-                    {
-                        let value = declaration[key];
-                        if (isMediaQuery(value)) target[key] = new mediaQuery(value);
-                        else if (isStyleBody(value)) target[key] = new styleBody(value);
-                        else if (isPlainObject(value)) target[key] = new styleBody(value as RHU.Style.BodyDeclaration);
-                        // TODO(randomuserhi): Better error message => include full property path?
-                        else throw new Error(`Object assigned to ${key} is not a valid style object.`);
-                    }     
-                };
-                clone(this, declaration);
-            } as Function as { 
-                new(body: RHU.Style.BlockDeclaration): MediaQuery;
-                prototype: MediaQuery;
-            };
-            let isMediaQuery: (obj: any) => obj is MediaQuery 
-                = Object.isPrototypeOf.bind(mediaQuery.prototype);
-
-            let element = Symbol("Element reference");
-            RHU.Style.el = function() { return element; };
-
-            // NOTE(randomuserhi): The below is for testing purposes and not to push to prod
+            // Test code:
+            type CSSStyle<T extends RHU.Style.CSSStyle = {}> = RHU.Style.CSSStyle<T>;
             let style = RHU.Style!<{
-                properties: CSSBody<{
-                    cringe: CSSBody
+                button: CSSStyle<{
+                    text: CSSStyle
                 }>,
-                children: {
-                    cringe: CSSBody
-                }
-                wrapper: CSSBody,
-                grid: {
-                    cell: CSSBody
-                },
-                display: {
-                    display: CSSBody
-                },
-                // grid: CSSBlock<{ ... }> also works
-                query: CSSMediaQuery
+                /*query: CSSQuery<{
+                    mobile: CSSStyle
+                }>*/
             }>({
-                children: {
-                    properties: {
-                        display: "flex",
-                        cringe: {
-                            display: "flex"
-                        }
+                button: {
+                    __style__: {
+                        display: "flex"
                     },
-                    children: {
-                        cringe: {
+                    text: {
+                        __style__: {
                             display: "flex"
                         }
                     }
                 },
-                wrapper: {
-                    display: "flex"
-                },
-                grid: {
-                    cell: {
-                        display: "flex"
-                    }
-                },
-                display: {
-                    display: {
-                        display: "flex"
-                    }
-                },
-                query: RHU.Style!.MediaQuery({})
+                // the structure can either be defined here in <> or above in the parent
+                /*query: RHU.Style.mediaQuery({
+                
+                })*/
             });
         }
     }));
