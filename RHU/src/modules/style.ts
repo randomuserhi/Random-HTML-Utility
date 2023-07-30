5431da7b3b10cffecae2b47949548d800ee30a27
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-07-30 23:55:53 +0100

    Updated schema

--- RHU/src/modules/style.ts
+++ RHU/src/modules/style.ts
@@ -20,7 +20,21 @@
 
             // Type aliases for private symbol properties
 
-            // TODO(randomuserhi): Allow RHU.mediaquery(query, {}) and RHU.style({}) shorthands instead of __style__
+            // TODO(randomuserhi): Change type inference so that the returned type from RHU.Style and RHU.mediaQuery etc...
+            //                     only accepts classes for use in html
+            //                     Given the example below you should not be able to do:
+            //                     `style.button[":hover"]` ...
+            //                     You also should not be able to do
+            //                     `style.button.__style__.display = "none"`
+            //                     
+            //                     These actions are only prohibitted outside the generator function, thus to make it work I simply
+            //                     need to implement a type that can convert an intermediate type:
+            //                     `{ button: DeclCSSStyle<{}>, query: DeclMediaQuery<{}> }` to `{ button: CSSStyle, query: MediaQuery }` where 
+            //                     CSSStyle / MediaQuery is the internally used type for the generator to have access to __style__ and __query__ 
+            //                     properties and DeclCSSStyle / DeclMediaQuery is used externally for exposing specific classes
+            //
+            //                     This makes the behaviour defined in the sense that styles can't be altered post creation, and the exposed classes
+            //                     are simply the string names to be used in html: <div style="`${style.button}`"></div>
 
             // Test code:
             type CSSStyle<T extends {} = {}> = RHU.Style.CSSStyle<T>;
@@ -33,6 +47,9 @@
                     mobile: CSSStyle
                 }>
             }>((style) => {
+                let common: RHU.Style.StyleDeclaration = {
+                    color: "aliceblue"
+                }
                 style.button = {
                     __style__: {
                         display: "flex",
@@ -44,6 +61,9 @@
                         __style__: {
                             display: "block"
                         }
+                    },
+                    ":hover": {
+                        __style__: common
                     }
                 }
                 // NOTE(randomuserhi): <T> can't be inferred
@@ -61,9 +81,7 @@
                 style.query = {
                     __query__: "cringe",
                     mobile: {
-                        __style__: {
-
-                        }
+                        __style__: common
                     }
                 }
             });
