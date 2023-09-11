e3e4b69b1a47509dcb22053c1ccb7cabf744123d
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-09-11 21:33:02 +0100

    refactor old style system

--- RHU/types/modules/style.d.ts
+++ RHU/types/modules/style.d.ts
@@ -12,62 +12,11 @@ declare namespace RHU
 {
     interface Style
     {
-        <T extends Style.RestrictProps<T> & Style.DeclarationSchema<Style.CSSAll>>(generator: (root: T) => void): ReadOnly<T>;
-        mediaQuery<T extends Style.RestrictProps<T> & Style.DeclarationSchema<Style.CSSAll>>(generator: (root: Style.CSSMediaQuery<T>) => void): Style.CSSMediaQuery<T>;
-
-        el<Tag extends keyof HTMLElementTagNameMap>(tag: Tag): symbol; 
-        /** @deprecated */
-        el<Tag extends keyof HTMLElementDeprecatedTagNameMap>(tag: Tag): symbol; 
-        el(tag: string): symbol; 
+        
     }
 
     namespace Style
     {
-        // Utility types
-        interface StyleTypeMap
-        {
-            "CLASS": CSSStyle;
-            "MEDIA_QUERY": CSSMediaQuery;
-        }
-        type CSSAll = StyleTypeMap[keyof StyleTypeMap];
-
-        type TypesIn<T> = T[keyof T];
-        type DeclarationObject<T> = T & {
-            [Property: string]: TypesIn<T> | CSSAll | undefined; 
-        }
-        type DeclarationSchema<AcceptedTokens = CSSStyle> = {
-            [Property in string]?: AcceptedTokens
-        }
-
-        // NOTE(randomuserhi): Special properties in styles utilize `__${string}__` pattern, the following are utility types for
-        //                     handling them.
-        type RestrictProps<T> = {
-            [K in keyof T]: K extends `__${string}__` ? never : T[K];
-        }
-        type StripRestrictedProps<T> = {
-            [K in keyof T as K extends `__${string}__` ? never : K]: T[K];
-        }
-        type CSSToken<Schema, Token> = StripRestrictedProps<Schema> & DeclarationObject<Token>;
-
-        // Declaration Types
-        interface CSSStyleProperties
-        {
-            __type__?: "CLASS";
-            __style__?: StyleDeclaration;
-        }
-        //type CSSStyle<T extends DeclarationSchema = {}> = Omit<T, keyof CSSStyleProperties> & DeclarationObject<CSSStyleProperties>; /* deprecated type definition */
-        //type CSSStyle<T extends DeclarationSchema = {}> = Omit<StripRestrictedProps<T>, keyof CSSStyleProperties> & DeclarationObject<CSSStyleProperties>; /* deprecated type definition */
-        type CSSStyle<T extends DeclarationSchema = {}> = CSSToken<T, CSSStyleProperties>;
-        
-        interface CSSMediaQueryProperties
-        {
-            __type__: "MEDIA_QUERY";
-            __query__?: string;
-        }
-        //type CSSMediaQuery<T extends DeclarationSchema<CSSStyle | CSSMediaQuery> = {}> = Omit<T, keyof (CSSStyleProperties | CSSMediaQueryProperties)> & DeclarationObject<CSSMediaQueryProperties>; /* deprecated type definition */
-        //type CSSMediaQuery<T extends DeclarationSchema<CSSStyle | CSSMediaQuery> = {}> = Omit<StripRestrictedProps<T>, keyof (CSSStyleProperties | CSSMediaQueryProperties)> & DeclarationObject<CSSMediaQueryProperties>; /* deprecated type definition */
-        type CSSMediaQuery<T extends DeclarationSchema<CSSStyle | CSSMediaQuery> = {}> = CSSToken<T, CSSMediaQueryProperties>;
-
         type StyleDeclaration = {
             [Property in Style.CSSProperty]?: Property extends keyof Style.CSSPropertiesMap ? Style.CSSPropertiesMap[Property] : CSSValue;
         };
