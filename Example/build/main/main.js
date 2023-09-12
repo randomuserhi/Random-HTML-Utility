4873edb520a8cdd1ef5c4e18d2579aba8c41465b
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-09-12 18:23:07 +0100

    Fixed example

--- Example/build/main/main.js
+++ Example/build/main/main.js
@@ -28,26 +28,3 @@ RHU.module(new Error(), "Main", { Style: "rhu/style", Macro: "rhu/macro" }, func
         element: `<div class="${style.wrapper}"></div>`
     });
 });
-RHU.module(new Error(), "Main2", { Style: "rhu/style", Macro: "rhu/macros" }, function ({ Style, Macro }) {
-    const style = Style(({ style, css }) => {
-        const wrapper = style.class `
-            display: flex;
-            gap: 10px;
-            width: 100%;
-            height: 100%;
-            background-color: blue;
-            padding: 10px;
-            `;
-        style `
-            ${wrapper}>div {
-                width: 100%;
-                height: 10px;
-                background-color: white;
-            }
-            `;
-        return {
-            wrapper
-        };
-    });
-    console.log(Macro);
-});
--- Example/src/main/main.ts
+++ Example/src/main/main.ts
@@ -61,34 +61,3 @@ RHU.module(new Error(), "Main",
             });
     }
 );
-
-RHU.module(new Error(), "Main2" as RHU.Module.Exports, 
-    { Style: "rhu/style", Macro: "rhu/macros" as RHU.Module.Exports },
-    function({ Style, Macro })
-    {
-        const style = Style(({ style, css }) => {
-            const wrapper = style.class`
-            display: flex;
-            gap: 10px;
-            width: 100%;
-            height: 100%;
-            background-color: blue;
-            padding: 10px;
-            `;
-
-            style`
-            ${wrapper}>div {
-                width: 100%;
-                height: 10px;
-                background-color: white;
-            }
-            `;
-
-            return {
-                wrapper
-            }
-        });
-
-        console.log(Macro);
-    }
-);
