75722130631d33d6cf73d13e3588de4ef306ae8a
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-09-13 20:16:15 +0100

    Revert "Delete Example/build directory"
    This reverts commit 24fe0acd3fff0d6ac81e2a7b199167a26a072d1b.

---
+++ Example/build/js3party/RHU/modules/style.js
@@ -0,0 +1,99 @@
+(function () {
+    let RHU = window.RHU;
+    if (RHU === null || RHU === undefined)
+        throw new Error("No RHU found. Did you import RHU before running?");
+    RHU.module(new Error(), "rhu/style", {}, function () {
+        let id = 69;
+        let cn = function (name) {
+            if (RHU.exists(name)) {
+                this.name = name;
+            }
+            else {
+                this.name = `rhu-${++id}`;
+            }
+        };
+        cn.prototype[Symbol.toPrimitive] = function () {
+            return this.name;
+        };
+        let interpret = function (value) {
+            if (typeof value === "number") {
+                return `${value}px`;
+            }
+            return value;
+        };
+        let css = function (style) {
+            let result = "";
+            for (const [key, value] of Object.entries(style)) {
+                let prop = key.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
+                if (typeof value === "string" || value instanceof String) {
+                    result += `${prop}:${value};`;
+                }
+                else if (typeof value === "number") {
+                    result += `${prop}:${value}px;`;
+                }
+                else {
+                    switch (prop) {
+                        case "border":
+                            const parse = value;
+                            const borderRadius = interpret(parse["border-radius"] || parse.borderRadius);
+                            if (RHU.exists(borderRadius))
+                                result += `border-radius: ${borderRadius}; `;
+                            break;
+                    }
+                }
+            }
+            return result;
+        };
+        let Style = function (factory) {
+            let generatedCode = "";
+            let generator = function (first, ...interpolations) {
+                generatedCode += first[0];
+                for (let i = 0; i < interpolations.length; ++i) {
+                    const interpolation = interpolations[i];
+                    if (typeof interpolation === "object") {
+                        if (interpolation instanceof cn) {
+                            generatedCode += `.${interpolation}`;
+                        }
+                        else {
+                            generatedCode += css(interpolation);
+                        }
+                    }
+                    else {
+                        generatedCode += interpolation;
+                    }
+                    generatedCode += first[i + 1];
+                }
+            };
+            generator.class = function (first, ...interpolations) {
+                const classname = new cn();
+                if (first.length > 1 || first[0] !== '' || interpolations.length !== 0) {
+                    generatedCode += `.${classname} {${first[0]}`;
+                    for (let i = 0; i < interpolations.length; ++i) {
+                        const interpolation = interpolations[i];
+                        if (typeof interpolation === "object") {
+                            if (interpolation instanceof cn) {
+                                generatedCode += interpolation;
+                            }
+                            else {
+                                generatedCode += css(interpolation);
+                            }
+                        }
+                        else {
+                            generatedCode += interpolation;
+                        }
+                        generatedCode += first[i + 1];
+                    }
+                    generatedCode += "}";
+                }
+                return classname;
+            };
+            const exports = factory({ style: generator, css, cn: () => new cn() });
+            generatedCode = generatedCode.replace(/(\r\n|\n|\r)/gm, "").replace(/ +/g, ' ').trim();
+            let el = document.createElement("style");
+            el.innerHTML = generatedCode;
+            document.head.append(el);
+            return exports;
+        };
+        return Style;
+    });
+})();
